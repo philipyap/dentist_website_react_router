7075f781e7004c993f1fa626c63b0dfc3e1ea45a
Philip Yap <philipyap@Philips-MacBook.local>
2020-08-28 10:53:19 -0700

    [react-dentist]

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Home extends Component {
+    render(){
+        return(
+            <div>
+            <h1>Philip Dentistry</h1>
+            <p>Welcome to visit us !</p>
+            </div>
+        )
+    }
+}
+export default Home

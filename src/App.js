b03eb62e59a3c0853c1b25b279af2c4f5259af0a
Philip Yap <philipyap@Philips-MacBook.local>
2020-08-28 11:30:13 -0700

    [react-dentist]added procedures

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-
+// dom library
 import {
   BrowserRouter as Router,
   Route,
@@ -12,20 +12,30 @@ import Procedures from './Procedures'
 import Contact from './Contact'
 
 function App() {
+  const procedures= [
+    "Deep Cleaning",
+    "Oral Surgery",
+    "Root Canal",
+    "Crown",
+    "Implante",
+    "Fillings"
+  ]
+
   return (
     <Router>
       <div className="App">
         <nav>
-          <Link to="/">Go Back to Home</Link>{'  '}
+          {/* {' '}space to separate from links */}
+          <Link to="/">Go Back to Home</Link>{'  '} 
           <Link to="/procedures">See our Procedures</Link>{'  '}
           <Link to="/contact">Contact Us</Link>{' '}
         </nav>
         {/* *exact on home route  */}
-       <Route exact path='/' component={Home}/>
-       <Route path='/procedures' component={Procedures} />
+        <Route exact path='/' component={Home}/>
+        <Route path='/procedures' render={()=> <Procedures procedures={procedures}/>} />
        {/* needs *exact if it has route within a route
-       <Route exact path='/procedures/1' /> */}
-       <Route path='/contact' component={Contact} />
+        <Route exact path='/procedures/1' /> */}
+        <Route path='/contact' component={Contact} />
       </div> 
     </Router>
   );
--- src/Procedures.js
+++ src/Procedures.js
@@ -2,9 +2,13 @@ import React, {Component} from 'react'
 
 class Procedures extends Component{
     render(){
+        const procedureList = this.props.procedures.map((p, idx)=> {
+            return <li key={idx}>{p}</li>
+        })
         return(
             <>
             <h1>Procedures</h1>
+            {procedureList}
             </>
         )
     }
